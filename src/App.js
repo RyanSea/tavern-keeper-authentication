78b4c89ad83baa13ce6cef92671e028fe864f444
Ryan <81717668+RyanSea@users.noreply.github.com>
2022-06-26 00:17:07 -0700

    Update ✨

--- src/App.js
+++ src/App.js
@@ -60,8 +60,8 @@ function App() {
           }
           
         }
-        console.log(server_id)
-        valu.authenticate(server_id, user.id, Address)
+        
+        await valu.authenticate(server_id, user.id, Address)
         document.getElementById("output").innerHTML = `Linked ${user.username}#${user.discriminator}'s Discord ID (${user.id}) to Wallet Address ${Address}`
         
     
